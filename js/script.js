3313fa40d0e409e66355f68e4f1e9f7084ab1e83
legacywebhub <legacywebhub@gmail.com>
2022-09-26 00:20:13 +0100

    Version II commit

--- js/script.js
+++ js/script.js
@@ -5,10 +5,45 @@ const closePopupBtn = document.querySelector('#close-popup'),
     searchForm = document.querySelector('#search-form'),
     randomSection = document.querySelector('#random-section'),
     randomRecipes = document.querySelector('#random-recipes'),
-    favouriteFoodContainer = document.querySelector('.favorite-foods-container');
+    favoriteFoodContainer = document.querySelector('.favorite-foods-container');
 
 
 // Functions
+let getFavoriteMealsFromLocalStorage = () => {
+    // get meal IDs from local storage
+    // local storage items are stored as strings hence we convert to JSON
+    let mealIds = JSON.parse(localStorage.getItem('mealIds'));
+
+
+    if (mealIds == null) {
+        // if there is no meal IDs in local storage then set one
+        localStorage.setItem('mealIds', JSON.stringify([]));
+    }
+
+    // return meal IDs
+    return mealIds;
+}
+
+
+
+let addFavoriteMealToLocalStorage = (mealId) => {
+    // get meal IDs
+    let mealIds = getFavoriteMealsFromLocalStorage();
+    // add passed argument to meal IDs
+    localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealId]));
+}
+
+
+
+let removeFavoriteMealFromLocalStorage = (mealId) => {
+    // get meal IDs
+    let mealIds = getFavoriteMealsFromLocalStorage();
+    // add passed argument from meal IDs
+    localStorage.setItem('mealIds', JSON.stringify(mealIds.filter(meal => meal !== mealId)));
+}
+
+
+
 let loadDOM = (mealData) => {
     let meal = document.createElement('div');
     meal.classList.add('random-recipe');
@@ -18,51 +53,79 @@ let loadDOM = (mealData) => {
     <p class="random-meal-detail"><span id="${mealData.idMeal}" class="random-meal-name">${mealData.strMeal}</span> <span><button class="fav-btn" id="${mealData.idMeal}"><i class="fa fa-heart"></i></button></span></p>
     `;
     randomRecipes.appendChild(meal);
-    console.log('Dom mounted');
 }
+
+
 
 let fetchRandomMeals = async ()  => {
-    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
-    let data = await response.json();
-    let meals = data.meals;
 
-    // reseting our random section to upload results
-    randomSection.querySelector('#random-header').innerText = `Random Meal(s)`;
-    randomRecipes.innerHTML = '';
+    //load our animation
+    randomSection.querySelector('#random-header').innerText = `Fetching random meal...`;
+    randomRecipes.innerHTML = '<img class="loading-image" src="images/loading.gif">';
 
-    for(let i = 0; i < meals.length; i++) {
-        loadDOM(meals[i]);
+    try {
+
+        // fetch data from API
+        let response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
+        let data = await response.json();
+        let meals = data.meals;
+    
+        // reseting our random section to upload results
+        randomSection.querySelector('#random-header').innerText = `Random Meal`;
+        randomRecipes.innerHTML = '';
+    
+        for(let i = 0; i < meals.length; i++) {
+            loadDOM(meals[i]);
+        }
+    
+        // listening for favorite button clicks of generated items
+        document.querySelectorAll('.fav-btn').forEach((btn)=>{
+        
+            btn.addEventListener('click', ()=>{
+                if (btn.classList.contains('active')) {
+                    // remove meal ID from local storage
+                    removeFavoriteMealFromLocalStorage(btn.id);
+                    // remove active class from button
+                    btn.classList.remove('active');
+                } else {
+                    // add meal ID to local storage
+                    addFavoriteMealToLocalStorage(btn.id);
+                    // add active class to button
+                    btn.classList.add('active');
+                }
+    
+                // reloading our favorite meals container
+                fetchFavoriteMeals();
+            });
+    
+        });
+    
+        // listening for popup clicks of generated items
+        document.querySelectorAll('.popup').forEach((item)=>{
+            item.addEventListener('click', ()=>{
+                showMealInfo(item.id);
+            })
+        });
+    
+    } catch {
+        // if errors
+        setTimeout(()=>{
+            randomSection.querySelector('#random-header').innerText = `Couldn't fetch random meal`;
+            randomRecipes.innerHTML = `<img class="sorry-animation" src="images/sorry.gif">`;
+        }, 5000);
     }
 
-    // listening for favorite button clicks of generated items
-    document.querySelectorAll('.fav-btn').forEach((btn)=>{
-    
-        btn.addEventListener('click', ()=>{
-            if (btn.classList.contains('active')) {
-                removeFavoriteMealFromLocalStorage(btn.id);
-                btn.classList.remove('active');
-            } else {
-                addFavoriteMealToLocalStorage(btn.id);
-                btn.classList.add('active');
-            }
-
-            // reloading our favorite meals container
-            fetchFavouriteMeals();
-        });
-
-    });
-
-    // listening for popup clicks of generated items
-    document.querySelectorAll('.popup').forEach((item)=>{
-        item.addEventListener('click', ()=>{
-            showMealInfo(item.id);
-        })
-    });
 }
+
+
 
 let  searchMeal = async () => {
     let search = document.querySelector('#search-input').value,
     url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+
+    // loading animation
+    randomSection.querySelector('#random-header').innerText = `Searching ${search} recipes...`;
+    randomRecipes.innerHTML = `<img class="loading-image" src="images/loading.gif">`; 
 
     try {
         // fetching from API
@@ -71,131 +134,132 @@ let  searchMeal = async () => {
         const data = await ourRequest.json();
 
         if (data.meals) {
-            const meals = data.meals;
+            // if there was returned data
+
+            // this lets our animation to load for a while
+            // before updating our DOM
+            setTimeout(()=>{
+                const meals = data.meals;
     
-            // reseting our random section to upload results
-            randomSection.querySelector('#random-header').innerText = `Search results for ${search}`;
-            randomRecipes.innerHTML = '';
+                // reseting our random section to upload results
+                randomSection.querySelector('#random-header').innerText = `Search results for ${search}`;
+                randomRecipes.innerHTML = '';
+        
+                // looping through meals results
+                for(let i = 0; i < meals.length; i++) {
+                    loadDOM(meals[i]);
+                }
+        
+                // listening for favorite button clicks of generated items
+                document.querySelectorAll('.fav-btn').forEach((btn)=>{
+                    
+                    btn.addEventListener('click', ()=>{
+                        if (btn.classList.contains('active')) {
+                            removeFavoriteMealFromLocalStorage(btn.id);
+                            btn.classList.remove('active');
+                        } else {
+                            addFavoriteMealToLocalStorage(btn.id);
+                            btn.classList.add('active');
+                        }
+        
+                        // reloading our favorite meals container
+                        fetchFavoriteMeals();
+                    });
     
-            // looping through meals results
-            for(let i = 0; i < meals.length; i++) {
-                console.log('loop '+ i)
-                loadDOM(meals[i]);
-            }
-    
-            // listening for favorite button clicks of generated items
-            document.querySelectorAll('.fav-btn').forEach((btn)=>{
-                
-                btn.addEventListener('click', ()=>{
-                    if (btn.classList.contains('active')) {
-                        removeFavoriteMealFromLocalStorage(btn.id);
-                        btn.classList.remove('active');
-                    } else {
-                        addFavoriteMealToLocalStorage(btn.id);
-                        btn.classList.add('active');
-                    }
-    
-                    // reloading our favorite meals container
-                    fetchFavouriteMeals();
                 });
+    
+                // listening for popup clicks of generated items
+                document.querySelectorAll('.popup').forEach((item)=>{
+                    item.addEventListener('click', ()=>{
+                        showMealInfo(item.id);
+                    });
+                });
+            }, 5000)
 
-            });
+        } else {
+            // if no returned data or null
+            randomSection.querySelector('#random-header').innerText = `No results for ${search}`;
+            randomRecipes.innerHTML = `<img class="sorry-animation" src="images/sorry.gif">`;
+        }
 
+    } catch {
+        // if errors
+        setTimeout(()=>{
+        randomSection.querySelector('#random-header').innerText = `Sorry there was an error while fetching results`;
+        randomRecipes.innerHTML = '<img class="sorry-animation" src="images/sorry.gif">';
+        }, 3000);
+    }
+}
+
+
+
+let fetchFavoriteMeals = async () => {
+    favoriteFoodContainer.innerHTML = `<img style="width:3rem; margin-bottom:1rem;" src="images/loading2.gif">`;
+
+    let favoriteMeals = getFavoriteMealsFromLocalStorage()
+
+    try {
+        if (favoriteMeals.length <= 0 || favoriteMeals == null || !favoriteMeals.length) {
+            favoriteFoodContainer.innerHTML = `<h4 class="no-favorite">You have no favorite meals yet</h4>`;
+        } else {
+            // clearing animation
+            favoriteFoodContainer.innerHTML = ``;
+
+            for(let i=0; i<favoriteMeals.length; i++) {
+                // fetch data for each meal ID
+                const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${parseInt(favoriteMeals[i])}`);
+                const data = await response.json();
+                const meal = data.meals
+    
+                // generate DOM
+                let favoriteFoodItem = document.createElement('div');
+                favoriteFoodItem.classList.add('favorite-food-item');
+        
+                favoriteFoodItem.innerHTML = `
+                <button class="remove-favorite-button" id="${meal[0].idMeal}"><i class="fa fa-times"></i></button>
+                <img class="popup" id="${meal[0].idMeal}" src="${meal[0].strMealThumb}" alt="meal image">
+                <p class="popup" id="${meal[0].idMeal}" title="${meal[0].strMeal}">${meal[0].strMeal}</p>
+                `
+        
+                favoriteFoodContainer.appendChild(favoriteFoodItem);
+            }
+        
+            // animation to display or hide remove button
+            document.querySelectorAll('.favorite-food-item').forEach((item)=> {
+                item.addEventListener('mouseover', ()=>{
+                    let removeButton = item.querySelector('.remove-favorite-button');
+                    removeButton.style.opacity = 0.8;
+                })
+                item.addEventListener('mouseout', ()=>{
+                    let removeButton = item.querySelector('.remove-favorite-button');
+                    removeButton.style.opacity = 0;
+                })
+            })
+    
+            // listening for remove favorite button clicks
+            document.querySelectorAll('.remove-favorite-button').forEach((button)=>{
+                button.addEventListener('click', ()=>{
+                    removeFavoriteMealFromLocalStorage(button.id);
+                    fetchFavoriteMeals();
+                })
+            })
+    
             // listening for popup clicks of generated items
             document.querySelectorAll('.popup').forEach((item)=>{
                 item.addEventListener('click', ()=>{
                     showMealInfo(item.id);
-                });
+                })
             });
-
-        } else {
-            randomSection.querySelector('#random-header').innerText = `No results for ${search}`;
-            randomRecipes.innerHTML = '';
         }
-
     } catch {
-        setTimeout(10000, ()=>{
-            // reseting our random section to upload results
-            randomSection.querySelector('#random-header').innerText = `Sorry there was an error while fetching results`;
-            randomRecipes.innerHTML = '';
-        })
+        // if errors
+        setTimeout(()=>{
+            favoriteFoodContainer.innerHTML = `<h4 class="no-favorite">Sorry.. there was an error while fetching favorite meals. Try refreshing your browser!</h4>`;
+        }, 3000);
     }
 }
 
-let getFavoriteMealsFromLocalStorage = () => {
-    let mealIds = JSON.parse(localStorage.getItem('mealIds'));
 
-    if (mealIds == null) {
-        localStorage.setItem('mealIds', JSON.stringify([]));
-    }
-
-    return mealIds;
-}
-
-let addFavoriteMealToLocalStorage = (mealId) => {
-    let mealIds = getFavoriteMealsFromLocalStorage();
-    localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealId]));
-}
-
-let removeFavoriteMealFromLocalStorage = (mealId) => {
-    let mealIds = getFavoriteMealsFromLocalStorage();
-    localStorage.setItem('mealIds', JSON.stringify(mealIds.filter(meal => meal !== mealId)));
-}
-
-let fetchFavouriteMeals = async () => {
-    favouriteFoodContainer.innerHTML = ``;
-
-    let favoriteMeals = getFavoriteMealsFromLocalStorage()
-
-    if (favoriteMeals.length < 1 || favoriteMeals == null) {
-        favouriteFoodContainer.innerHTML = `<h5 class="no-favorite">You have no favorite meals yet</h5>`;
-    } else {
-        for(let i=0; i<favoriteMeals.length; i++) {
-            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${parseInt(favoriteMeals[i])}`);
-            const data = await response.json();
-            const meal = data.meals
-
-            let favoriteFoodItem = document.createElement('div');
-            favoriteFoodItem.classList.add('favorite-food-item');
-    
-            favoriteFoodItem.innerHTML = `
-            <button class="remove-favorite-button" id="${meal[0].idMeal}"><i class="fa fa-times"></i></button>
-            <img class="popup" id="${meal[0].idMeal}" src="${meal[0].strMealThumb}" alt="meal image">
-            <p class="popup" id="${meal[0].idMeal}" title="${meal[0].strMeal}">${meal[0].strMeal}</p>
-            `
-    
-            favouriteFoodContainer.appendChild(favoriteFoodItem);
-        }
-    
-        // animation to display or hide remove button
-        document.querySelectorAll('.favorite-food-item').forEach((item)=> {
-            item.addEventListener('mouseover', ()=>{
-                let removeButton = item.querySelector('.remove-favorite-button');
-                removeButton.style.opacity = 0.8;
-            })
-            item.addEventListener('mouseout', ()=>{
-                let removeButton = item.querySelector('.remove-favorite-button');
-                removeButton.style.opacity = 0;
-            })
-        })
-
-        // listening for remove favorite button clicks
-        document.querySelectorAll('.remove-favorite-button').forEach((button)=>{
-            button.addEventListener('click', ()=>{
-                removeFavoriteMealFromLocalStorage(button.id);
-                fetchFavouriteMeals();
-            })
-        })
-
-        // listening for popup clicks of generated items
-        document.querySelectorAll('.popup').forEach((item)=>{
-            item.addEventListener('click', ()=>{
-                showMealInfo(item.id);
-            })
-        });
-    }
-
-}
 
 let showMealInfo = async (meal)=>{
     let mealInfoContainer = document.querySelector('.meal-info');
@@ -204,14 +268,16 @@ let showMealInfo = async (meal)=>{
     popupContainer.classList.remove('hidden');
 
     // loading animation
-    mealInfoContainer.innerHTML = `<img src="images/loading.gif">`;
-    mealInfoContainer.classList.add('loading');
+    mealInfoContainer.innerHTML = `<img style="width:3rem" src="images/loading.gif">`;
+    mealInfoContainer.classList.add('loading-meal-animation');
 
     try {
+        // fetch data
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${parseInt(meal)}`);
         const data = await response.json();
         const mealData = data.meals;
 
+        // generate DOM
         mealInfoContainer.innerHTML = `
             <h2>${mealData[0].strMeal}</h2>
             <img src="${mealData[0].strMealThumb}" alt="">
@@ -223,12 +289,11 @@ let showMealInfo = async (meal)=>{
         `
 
         // removing loading animation style
-        mealInfoContainer.classList.remove('loading');
+        mealInfoContainer.classList.remove('loading-meal-animation');
 
+        // looping through 1 to 20 to get all ingredients from API data then update DOM
         for(let i=1;i <= 20;i++) {
-            console.log('entered loop')
             if(mealData[0]["strIngredient"+i]){
-                console.log(`condition ${i} met`);
                 let li = document.createElement('li');
                 li.innerHTML = `<li class="ingredient">${mealData[0]["strIngredient"+i]} - ${mealData[0]["strMeasure"+i]}</li>`;
                 mealInfoContainer.querySelector('#ingredients').appendChild(li);
@@ -237,19 +302,21 @@ let showMealInfo = async (meal)=>{
             }
         }
 
-
     } catch {
-        setTimeout(10000, ()=>{
-            mealInfoContainer.innerHTML = `<h5 class="meal-info-error">Sorry there was an error while fetching meal data</h5>`;
-            mealInfoContainer.classList.add('loading');
-        });
+        // if errors
+        setTimeout(()=>{
+            mealInfoContainer.innerHTML = `
+            <h4 class="meal-info-error">Sorry there was an error while fetching meal data</h4>
+            <img style="width: 15rem" src="images/sorry.gif">
+            `;
+            mealInfoContainer.classList.add('loading-meal-animation');
+        }, 3000);
     }
 
 }
 
 
 // Event Listeners
-
 closePopupBtn.addEventListener('click', ()=>{
     popupContainer.classList.add('hidden');
 })
@@ -261,5 +328,6 @@ searchForm.addEventListener('submit', async (e)=>{
 
 
 // Code blocks
-fetchFavouriteMeals();
+fetchFavoriteMeals();
 fetchRandomMeals();
+setInterval(()=> fetchRandomMeals(), 20000);
